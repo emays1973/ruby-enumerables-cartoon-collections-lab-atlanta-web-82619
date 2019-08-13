9d625ea4b9b16789eafbab58a015f95a70cfb6d9
Eric Mays <emays1973@gmail.com>
2019-08-13 16:51:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each do 
+    { |item| puts item }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map(&:upcase).join("!")
+  puts array
 end
 
 def long_planeteer_calls# code an argument here
